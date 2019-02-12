a3b211d71920baa0f9fe6ac66fbe2bd4ee294853
jvsena42 <jvsena16@gmail.com>
2019-02-12 09:08:24 -0500

    +Botao sair

--- app/src/main/java/com/app/organizze/organizze/activity/PrincipalActivity.java
+++ app/src/main/java/com/app/organizze/organizze/activity/PrincipalActivity.java
@@ -6,6 +6,8 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -21,13 +23,14 @@ public class PrincipalActivity extends AppCompatActivity {
 
     private MaterialCalendarView calendarView;
     private TextView textoSaudacao,textoSaldo;
+    private FirebaseAuth autenticacao;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         calendarView = findViewById(R.id.calendarView);
@@ -44,7 +47,27 @@ public class PrincipalActivity extends AppCompatActivity {
         });*/
 
         configuraCalendarView();
-        
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_principal,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menuSair:
+                autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+                autenticacao.signOut();
+                startActivity(new Intent(this,MainActivity.class));
+                finish();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     public void adicionarDespesa(View view){
